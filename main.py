c94be1abe273aef4bfd7fe3fe95e8be8610a869a
tuzoep <sas.peter.hu@gmail.com>
2022-08-05 14:39:44 +0200

    Delete unused code

--- main.py
+++ main.py
@@ -18,14 +18,10 @@ def color_count(filename):
     """Takes the filename as an argument and returns the top 10 colors of the image"""
     im = Image.open(f"{UPLOAD_FOLDER}/{filename}")
     im_np = np.array(im)
-    # print(f"The type of the image: {type(im_np)}")
     print(f"The shape of the image: {np.shape(im_np)}")
     im_width = np.shape(im_np)[0]
     im_height = np.shape(im_np)[1]
-    # print(im_np)
-    # print(im.format, im.size, im.mode)
     pix = im.load()
-    # print(pix[500, 500])
     colors_rgb = []
     for x in range(0, im_width):
         for y in range(0, im_height):
@@ -33,7 +29,6 @@ def color_count(filename):
 
     count = pd.Series(colors_rgb).value_counts()
     top_10_colors = count[:10]
-    # print(count[:10])
     hex_list = []
     for i in top_10_colors.index:
         hex_list.append(f"{'#%02x%02x%02x' % i}")
@@ -44,16 +39,6 @@ def color_count(filename):
     }
     return color_dict
 
-
-def rgb_to_hex(list):
-    """Takes a list of RGB tuples and converts them to hexademical strings. Returns a list"""
-    hex_list = []
-    for i in list:
-        hex_list.append('#%02x%02x%02x' % i)
-    return hex_list
-
-# test = color_count("66720568_a_1.jpg")
-# print(test)
 
 def allowed_file(filename):
     """Checks if the extension is valid"""
@@ -81,7 +66,6 @@ def home():
             filename = secure_filename(file.filename)
             file.save(os.path.join(UPLOAD_FOLDER, filename))
             colors = color_count(filename)
-            # colors_hex = rgb_to_hex(colors)
             image_to_display = f"{UPLOAD_FOLDER}/{filename}"
     return render_template("index.html", color_list=colors, image_location=image_to_display)
 
